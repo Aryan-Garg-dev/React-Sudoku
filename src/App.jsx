70ab160fe66d5bef35feabbd2fb8209c83adc250
Aryan-Garg-dev <aryangarg3010@gmail.com>
2024-06-20 07:53:46 +0530

    Refined all the logic and restructured them

--- src/App.jsx
+++ src/App.jsx
@@ -8,11 +8,13 @@ import { gameStateAtom } from "./atoms";
 import Difficulty from "./Components/Difficulty";
 import { createNewGame } from "./functions";
 import _ from 'lodash'
+import { Toggle } from "./Components/Toggle";
 
 function App() {
 
   const [ game, setGame ] = useRecoilState(gameStateAtom)
 
+  // (Function to generate and set the new game state)
   const generateNewGame = useCallback(
     (difficulty) => {
       const { data, board, solution } = createNewGame(difficulty);
@@ -46,6 +48,7 @@ function App() {
     [setGame, game.selectedDifficulty]
   );
 
+  // (Initializing the Game)
   useEffect(() => {
     const savedGame = JSON.parse(localStorage.getItem("game"));
     if (
@@ -53,39 +56,107 @@ function App() {
       !savedGame.board.length ||
       !savedGame.isRunning
     ) {
-      const newGame = generateNewGame(game.selectedDifficulty);
+      const newGame = generateNewGame();
       localStorage.setItem("game", JSON.stringify(newGame));
     } else {
       setGame(savedGame);
     }
   }, [setGame]);
 
+  //Game Over
   useEffect(() => {
     if (
         game.board.length 
         && game.solution.length 
         && _.flattenDeep(game.board).join("") === _.flattenDeep(game.solution).join("")
       ) {
-      const newGame = {
+      setGame(game=>({
         ...game,
         isOver: true,
         isRunning: false,
-      };
-      setGame(newGame);
-      localStorage.setItem("game", JSON.stringify(newGame));
+      }));
     }
   }, [game.board, game.solution, setGame]);
 
+  //Difficulty (Change in difficulty)
   useEffect(() => {
     if (game.selectedDifficulty && game.selectedDifficulty != game.data.difficulty ) {
       generateNewGame(game.selectedDifficulty);
     }
   }, [game.selectedDifficulty, generateNewGame, game.data.difficulty]);
 
-  useEffect(() => {
-    if (game.isRunning && game.board.length)
-    localStorage.setItem('game', JSON.stringify(game));
-  }, [game]);
+
+//Numbers (function to check which number is completed on board)
+const isDisabled = useCallback(query=>{
+  const solution = _.flatten(game.solution);
+  const board = _.flatten(game.board);
+  return !solution.filter((num, index)=>num!=board[index]).includes(query)
+}, [game.board, game.solution])
+  
+//Numbers (array of disabled numbers)
+useEffect(()=>{
+    const disabledNumbers = _.range(1, 9).filter(num=>isDisabled(num));
+    if (disabledNumbers.length){
+        setGame(game=>({
+            ...game,
+            disabledNumbers,
+        }))
+    }
+}, [isDisabled, setGame, game.board])
+
+//Numbers (reseet selected squares and selected number if it is disabled)
+useEffect(()=>{
+    if (isDisabled(game.selectedNumber)){
+        setGame(game=>({
+            ...game,
+            selectedNumber: null,
+            selectedSquaresForNumber: [],
+        }))
+    }
+}, [isDisabled, setGame, game.selectedNumber]);
+
+//Numbers (move hints for selected square)
+useEffect(()=>{
+    const r = game.selectedSquare.r;
+    const c = game.selectedSquare.c;
+    if (r != null && c != null){
+        const validNumbers = _.range(1, 10);
+        for (let i = 0; i < 3; i++){
+            for (let j = 0; j < 3; j++){
+                const [ x, y ] = [ Math.floor(r/3)*3, Math.floor(c/3)*3 ];
+                if ((x+i != r || y+j != c) && game.board[x+i][y+j]){
+                    const index = validNumbers.indexOf(game.board[x+i][y+j]);
+                    validNumbers.splice(index, 1)
+                }
+            }
+        }
+        
+        for (let i = 0; i < 9; i++){
+            if (validNumbers.includes(game.board[i][c])){
+                const index =validNumbers.indexOf(game.board[i][c])
+                validNumbers.splice(index, 1)
+            }
+        }
+
+        for (let j = 0; j < 9; j++){
+            if (validNumbers.includes(game.board[r][j])){
+                const index =validNumbers.indexOf(game.board[r][j])
+                validNumbers.splice(index, 1)
+            }
+        }
+
+        setGame(game=>({
+            ...game,
+            validNumbersForSquare: validNumbers,
+        }))
+    }
+}, [game.board, game.selectedSquare, game.selectedSquare.c, game.selectedSquare.r, setGame])
+
+// (storing game state after every state changes)
+useEffect(() => {
+  if (game.board.length && (game.isRunning || game.isOver))
+  localStorage.setItem('game', JSON.stringify(game));
+}, [game]);
 
   return (
     <>
@@ -96,8 +167,9 @@ function App() {
             <Board />
             <Numbers />
           </div>
-          <div>
+          <div className="flex flex-col gap-4">
             <Difficulty />
+            <Toggle text={'Moves'} />
           </div>
         </div>
         {/* <Timer /> */}
--- src/Components/Numbers.jsx
+++ src/Components/Numbers.jsx
@@ -1,73 +1,9 @@
 import _ from 'lodash';
 import { useRecoilState } from 'recoil';
 import { gameStateAtom } from '../atoms';
-import { useCallback, useEffect } from 'react';
 
 const Numbers = () => {
     const [game, setGame] = useRecoilState(gameStateAtom);
-
-    const isDisabled = useCallback(query=>{
-        const solution = _.flatten(game.solution);
-        const board = _.flatten(game.board);
-        return !solution.filter((num, index)=>num!=board[index]).includes(query)
-    }, [game.board, game.solution])
-    
-    useEffect(()=>{
-        const disabledNumbers = _.range(1, 9).filter(num=>isDisabled(num));
-        if (disabledNumbers.length){
-            setGame(game=>({
-                ...game,
-                disabledNumbers,
-            }))
-        }
-    }, [isDisabled, setGame, game.board])
-
-    useEffect(()=>{
-        if (isDisabled(game.selectedNumber)){
-            setGame(game=>({
-                ...game,
-                selectedNumber: null,
-                selectedSquaresForNumber: [],
-            }))
-        }
-    }, [isDisabled, setGame, game.selectedNumber]);
-
-    useEffect(()=>{
-        // [X] Sqaure Checks [X] line checks
-        const r = game.selectedSquare.r;
-        const c = game.selectedSquare.c;
-        if (r != null && c != null){
-            const validNumbers = _.range(1, 10);
-            for (let i = 0; i < 3; i++){
-                for (let j = 0; j < 3; j++){
-                    const [ x, y ] = [ Math.floor(r/3)*3, Math.floor(c/3)*3 ];
-                    if ((x+i != r || y+j != c) && game.board[x+i][y+j]){
-                        const index = validNumbers.indexOf(game.board[x+i][y+j]);
-                        validNumbers.splice(index, 1)
-                    }
-                }
-            }
-            
-            for (let i = 0; i < 9; i++){
-                if (validNumbers.includes(game.board[i][c])){
-                    const index =validNumbers.indexOf(game.board[i][c])
-                    validNumbers.splice(index, 1)
-                }
-            }
-
-            for (let j = 0; j < 9; j++){
-                if (validNumbers.includes(game.board[r][j])){
-                    const index =validNumbers.indexOf(game.board[r][j])
-                    validNumbers.splice(index, 1)
-                }
-            }
-
-            setGame(game=>({
-                ...game,
-                validNumbersForSquare: validNumbers,
-            }))
-        }
-    }, [game.board, game.selectedSquare, game.selectedSquare.c, game.selectedSquare.r, setGame])
     
   return (
     <div className='flex w-fit gap-[4px] m-2'>
@@ -80,7 +16,7 @@ const Numbers = () => {
                     ? 'border-2 bg-red-200 border-red-400'
                     : game.validNumbersForSquare.includes(index+1) //highlightMoves
                         ? 'bg-[#FFEEA9] border-2 border-[#FF7D29] shadow-md shadow-slate-600 active:translate-y-0.5 hover:bg-[#FFD966] text-red-950'
-                        :  isDisabled(index+1)
+                        :  game.disabledNumbers.includes(index+1)
                             ? 'border-2 border-gray-300 bg-gray-200 shadow-none text-gray-600'
                             : game.selectedNumber == index+1
                                 ? 'shadow-none border-2 bg-slate-700 border-slate-600 text-white active:translate-y-0.5'
@@ -90,7 +26,7 @@ const Numbers = () => {
             
             key={index}
             onClick={()=>{
-                if (game.selectedSquare.r != null && game.selectedSquare.c != null && !isDisabled(index+1)){
+                if (game.selectedSquare.r != null && game.selectedSquare.c != null && !game.disabledNumbers.includes(index+1)){
                     const { r, c } = game.selectedSquare;
                     if (index+1 == game.solution[r][c]){
                         const newBoard = _.cloneDeep(game.board);
@@ -111,7 +47,7 @@ const Numbers = () => {
                         })
                     }
                 } else {
-                    if (!isDisabled(index+1)){
+                    if (!game.disabledNumbers.includes(index+1)){
                         if (game.selectedNumber != index + 1){
                             setGame({
                                 ...game,
